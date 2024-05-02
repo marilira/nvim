c39b4ea17ec7319c5af653e499ac898ecba66679
marilira - archlinux <mariliradev@gmail.com>
2024-05-02 09:30:42 -0300

    add keymaps for competitest

--- lua/settings/plugins/competitest.lua
+++ lua/settings/plugins/competitest.lua
@@ -3,5 +3,16 @@ return {
 	dependencies = "MunifTanjim/nui.nvim",
 	config = function()
 		require("competitest").setup()
+
+		-- set keymaps
+		local keymap = vim.keymap -- for conciseness
+		keymap.set("n", "<leader>cc", "<cmd>Competitest run<cr>", { desc = "Compile and run test cases" })
+		keymap.set(
+			"n",
+			"<leader>cr",
+			"<cmd>Competitest receive testcases<cr>",
+			{ desc = "Get testcases from extension" }
+		)
+		keymap.set("n", "<leader>cs", "<cmd>Competitest show_ui<cr>", { desc = "Show previous testcases" })
 	end,
 }
